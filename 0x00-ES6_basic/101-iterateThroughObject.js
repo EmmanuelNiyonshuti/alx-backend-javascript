53fc210b775611565c0a3d1aa86ce31b5d36ace1
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-08-06 14:16:21 +0200

    used for...of loop 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return Array.from(reportWithIterator).join('|');
+//   return Array.from(reportWithIterator).join('|');
+//   return reportWithIterator.join('|');
+  const res = [];
+  for (const employee of reportWithIterator) {
+    res.push(employee);
+  }
+  return res.join(' | ');
 }
