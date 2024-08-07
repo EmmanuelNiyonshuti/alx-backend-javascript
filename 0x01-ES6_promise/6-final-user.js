f0944290a1757bdfd4c2a9126c33121099c42872
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-08-07 19:03:20 +0200

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+  return Promise.allSettled([promise1, promise2])
     .then((results) => results.map((result) => {
       if (result.status === 'fulfilled') {
         return { status: result.status, value: result.value };
