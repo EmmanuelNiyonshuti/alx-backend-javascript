339cdaf5befbda5283ebb1751c382aa477a539f9
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-08-09 01:49:22 +0200

    solution to task 9 and 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+// implements a class named Car.
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    // creates a new instance of the class of the object it's called on.
+    const Obj = this.constructor;
+    return new Obj();
+  }
+}
