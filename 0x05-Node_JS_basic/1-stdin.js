eff46a263e8c79aa4f5259a7449bd10dd2c42cbf
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-10-02 03:06:22 +0200

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf-8');
+
+const isInteractive = process.stdin.isTTY;
+
+process.stdin.on('data', (input) => {
+  const name = input.trim();
+  console.log(`Your name is: ${name}`);
+
+  if (!isInteractive) {
+    process.stdin.end();
+  } else {
+    process.exit();
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
