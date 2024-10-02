720c0400244907becd93ca64496f48bc7a2906eb
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-10-02 11:42:23 +0200

    reading files

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,16 +1,31 @@
 #!/usr/bin/node
-const readFileSync = require("fs:readFileSync")
+// reads and parse a csv file
+
+const fs = require('fs');
+
 const countStudents = (path) => {
-    try{
-        const data = readFileSync(path, {encoding: "utf-8"});
-        const lines = data.split("/n").filter((line) => line.trim() !== "");
-        if (lines.length <= 1){
-            throw new Error();
-        }
-        console.log(`Number of students: ${lines.length - 1}`);
-    } catch(error) {
-        
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
     }
-}
+    const students = lines.slice(1);
+    console.log(`Number of students: ${students.length}`);
+    const fields = {};
+    students.forEach((student) => {
+      const [firstName, , , field] = student.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    });
+    for (const [field, studentList] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
 
 module.exports = countStudents;
