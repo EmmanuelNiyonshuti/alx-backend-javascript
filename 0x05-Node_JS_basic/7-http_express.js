0dd87fe88c9b6287109d751314ffc2716b3a488a
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-10-03 19:12:16 +0200

    7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,21 +1,54 @@
 #!/usr/bin/node
 
 const express = require('express');
-const countStudents = require('./3-read_file_async');
 
 const app = express();
 const port = 1245;
 
+
+// reads file asynchronously
+
+const fs = require('fs');
+const { promises } = require('stream');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      if (lines.length <= 1) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const students = lines.slice(1);
+      const fields = {};
+      students.forEach((student) => {
+        const [firstName, , , field] = student.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstName);
+      });
+      let output = `Number of students: ${students.length}\n`;
+      for (const [field, studentList] of Object.entries(fields)) {
+        output += `Number of students in ${field}: ${studentList.length}. LIST: ${studentList.join(', ')}\n`;
+      }
+      resolve(output);
+    });
+  });
+}
+
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 app.get('/students', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   countStudents(process.argv[2]).then((students) => {
-    res.send(`This is the list of our students\n ${students}`);
+    res.send(`This is the list of our students\n${students}`);
   }).catch((error) => {
-    res.send(`This is the list of our students\n ${error.message}`);
+    res.send(`This is the list of our students\n${error.message}`);
   });
 });
 
