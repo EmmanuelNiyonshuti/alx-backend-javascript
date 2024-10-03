fd998fbab680b8c1f3d776bd632fb93cf3659977
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-10-03 02:42:12 +0200

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -6,15 +6,15 @@ const countStudents = require('./3-read_file_async');
 const PORT = 1245;
 const DATABASE = process.argv[2];
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents(DATABASE)
-      .then((output) => {
-        res.end(`This is the list of our students\n ${output}`);
-      })
+    countStudents(DATABASE).then((output) => {
+      res.end(`This is the list of our students\n ${output}`);
+    })
       .catch((error) => {
+        res.statusCode = 404;
         res.end(`This is the list of our students\n ${error.message}`);
       });
   }
